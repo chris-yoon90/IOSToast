4a83638b8b9b0b5cf39275686496bb5c0d628cef
chris.yoon <chris.yoon@gluwa.com>
2017-05-05 14:33:29 -0700

    update ios view

--- index.ios.js
+++ index.ios.js
@@ -20,17 +20,6 @@ export default class IOSToast extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-
         <TouchableOpacity
           onPress={() => ToastManager.makeToast('Testing toast.')}
         >
